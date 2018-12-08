f1e77e7c6bc2f09bbcbf240641fe428378d50764
tangqingsong <tangqingsong@offcn.com>
2018-12-08 12:44:26 +0800

    修复后台常量重复定义问题

--- admin/index.php
+++ admin/index.php
@@ -5,16 +5,17 @@
  * OldCMS,site:http://www.oldcms.com
  */
 require '../init.php';
-if($user->adminLevel<=0) { die('Access Denied');
+if ($user->adminLevel <= 0) {
+    die('Access Denied');
 }
 
 define('ADMIN_PATH', dirname(__FILE__));
-define('TEMPLATE_PATH', dirname(__FILE__));
+!defined('TEMPLATE_PATH') && define('TEMPLATE_PATH', dirname(__FILE__));
+$do = Val('do', 'GET', 0);
+$dos = array('admin_index', 'admin_module');
 
-$do=Val('do', 'GET', 0);
-$dos=array('admin_index','admin_module');
-
-if(!in_array($do, $dos)) { $do='admin_index';
+if (!in_array($do, $dos)) {
+    $do = 'admin_index';
 }
-require ADMIN_PATH.'/source/'.$do.'.php';
+require ADMIN_PATH . '/source/' . $do . '.php';
 ?>
