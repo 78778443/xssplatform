843e924c8f952b15381e91a90b468ce0f166a7d3
linyuanjia <11072162@qq.com>
2021-11-30 15:24:58 +0800

    修复邮件发送

--- config.php
+++ config.php
@@ -1,38 +1,44 @@
-<?php 
-$config = array (
-  'dbHost' => '127.0.0.1:3309',
-  'dbUser' => 'root',
-  'dbPwd' => '123',
-  'database' => 'xssplatform',
-  'charset' => 'utf8',
-  'tbPrefix' => 'oc_',
-  'dbType' => 'mysql',
-  'register' => 'normal',
-  'mailauth' => false,
-  'urlroot' => 'http://xss.localhost',
-  'urlrewrite' => false,
-  'filepath' => '/Users/song/mycode/safe/privatexss/upload',
-  'fileprefix' => 'http://xss.localhost/upload',
-  'theme' => 'default',
-  'template' => 'default',
-  'show' => 
-  array (
-    'sitename' => 'XSS平台',
-    'sitedesc' => '',
-    'keywords' => '技术交流,程序员,设计,项目,创业,技术,网络安全,技术文章,1.2',
-    'description' => '',
-    'adminmail' => '',
-  ),
-  'point' => 
-  array (
-    'award' => 
-    array (
-      'publish' => 2,
-      'comment' => 2,
-      'invitereg' => 10,
-    ),
-  ),
-  'timezone' => 'Asia/Shanghai',
-  'expires' => 3600,
-  'debug' => true,
+<?php
+$config = array(
+    'dbHost' => '127.0.0.1',
+    'dbUser' => 'root',
+    'dbPwd' => '123456',
+    'database' => 'xssplatform',
+    'charset' => 'utf8',
+    'tbPrefix' => 'oc_',
+    'dbType' => 'mysql',
+    'register' => 'normal',
+    'mailauth' => false,
+    'urlroot' => 'http://lyj.xssplatform.net',
+    'urlrewrite' => false,
+    'filepath' => 'D:/phpstudy_pro/WWW/xssplatform/upload',
+    'fileprefix' => 'http://lyj.xssplatform.net/upload',
+    'theme' => 'default',
+    'template' => 'default',
+    'show' =>
+        array(
+            'sitename' => 'XSS平台',
+            'sitedesc' => '',
+            'keywords' => '技术交流,程序员,设计,项目,创业,技术,网络安全,技术文章,1.2',
+            'description' => '',
+            'adminmail' => '',
+        ),
+    'point' =>
+        array(
+            'award' =>
+                array(
+                    'publish' => 2,
+                    'comment' => 2,
+                    'invitereg' => 10,
+                ),
+        ),
+    'timezone' => 'Asia/Shanghai',
+    'expires' => 3600,
+    'debug' => true,
+    // 邮件配置（）
+    'email'=>[
+        'host'=>'', // 服务器地址 如：smtp.qq.com
+        'username'=>'', // 用户名
+        'password'=>'', // 授权码
+    ]
 );
--- source/function.php
+++ source/function.php
@@ -304,48 +304,34 @@ function SendMail($to='',$subject='',$body=''){
 //发送邮件
 function SendMail($to = '', $subject = '', $body = '')
 {
-    global $mailConfig;
     include_once ROOT_PATH . '/source/class/PHPMailer.class.php';
-    $fromlist = array(
-        array('host' => 'smtp.qq.com', 'user' => '123456@qq.com', 'pass' => '123456'),
-    );
-    shuffle($fromlist);//妈妈再也不用担心我学习了o(>﹏<)o 轮流使用邮箱发送
-
-    //go
+    $config = [];
+    require ROOT_PATH . '/config.php';
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
     try {
-        foreach ($fromlist as $curmail) {
-            $mail = new PHPMailer();
-            $mail->IsSMTP();
-            if (defined('z_dm') && z_dm >= 1) {
-                $mail->SMTPDebug = 2;
-            }                     // enables SMTP debug information (for testing)
-            else {
-                $mail->SMTPDebug = 0;
-            }                     // enables SMTP debug information (for testing)
-            $mail->SMTPAuth = true;
-            $mail->SMTPSecure = "ssl";                           //当邮件服务器未使用SSL的时候 注释本段
-            $mail->Port = 465;                                   //当邮件服务器未使用SSL的时候 端口改为25
-            $mail->From = $curmail['user'];
-            $mail->FromName = "饼干商城";
-            $mail->Host = $curmail['host'];
-            $mail->Username = $curmail['user'];
-            $mail->Password = $curmail['pass'];
-            $mail->CharSet = "UTF-8";
-            $mail->AddAddress($to, $to);
-            $mail->Subject = $subject;
-            $mail->AltBody = "To view the message, please use an HTML compatible email viewer!";
-            $mail->MsgHTML($body);
-            $mail->IsHTML(true);
-            $ret = $mail->Send();
+        //服务器配置
+        $mail->CharSet ="UTF-8";                     //设定邮件编码
+        $mail->SMTPDebug = 1;                        // 调试模式输出
+        $mail->isSMTP();                             // 使用SMTP
+        $mail->Host = $config['email']['host'];                // SMTP服务器
+        $mail->SMTPAuth = true;                      // 允许 SMTP 认证
+        $mail->Username = $config['email']['username'];                // SMTP 用户名  即邮箱的用户名
+        $mail->Password = $config['email']['password'];             // SMTP 密码  部分邮箱是授权码(例如163邮箱)
+        $mail->SMTPSecure = 'ssl';                    // 允许 TLS 或者ssl协议
+        $mail->Port = 465;                            // 服务器端口 25 或者465 具体要看邮箱服务器支持
+        $mail->setFrom($config['email']['username'], 'Mailer');  //发件人
+        $mail->addAddress($to, 'Joe');  // 收件人
+        $mail->addReplyTo($config['email']['username'], 'info'); //回复的时候回复给哪个邮箱 建议和发件人一致
 
-            if ($ret) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    } catch (phpmailerException $e) {
-        return 0;
+        $mail->isHTML(true);                                  // 是否以HTML文档格式发送  发送后客户端可直接显示对应HTML内容
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = $subject;
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return false;
     }
 }
 
